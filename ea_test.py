9986cdcbc3066bd62c2649dacb0fed6ee81df6eb
Sean Aubin <saubin@uwaterloo.ca>
2016-03-22 21:00:22 -0400

    update ea_test

--- ea_test.py
+++ ea_test.py
@@ -24,18 +24,18 @@ pattern_file_names = [
 ]
 
 output_dims = 2
-pattern_num = 2
-pattern_file_names = pattern_file_names[:2]
+pattern_num = 1
+pattern_file_names = pattern_file_names[:pattern_num]
 
 function_list = [
+    [
+        lambda x: np.cos(x)/2,
+        lambda x: np.sin(x)/2
+    ],
     [
         lambda x: x/2,
         lambda x: -x/2
     ],
-    [
-        lambda x: np.cos(np.pi*x)/2,
-        lambda x: np.sin(np.pi*x)/2
-    ]
 ]
 
 np.random.seed(3)
@@ -43,7 +43,6 @@ np.random.seed(3)
 
 model = nengo.Network()
 tau = 0.1
-model.config[nengo.Ensemble].neuron_type = nengo.LIFRate()
 ea_list = []
 with model:
     osc = nengo.Ensemble(n_neurons=1, dimensions=3, neuron_type=nengo.Direct())
@@ -71,17 +70,16 @@ with model:
     nengo.Connection(bump, osc[0])
 
     # what's stopping this from being a passthrough node?
-    readout = nengo.Ensemble(n_neurons=1, dimensions=1, neuron_type=nengo.Direct())
+    readout = nengo.Ensemble(n_neurons=1, dimensions=1, radius=np.pi, neuron_type=nengo.Direct())
     nengo.Connection(osc[:2], readout, synapse=None,
                      function=lambda x: np.arctan2(x[1], x[0]))
 
     # controllers
-    inhibit_control = nengo.Node([0]*pattern_num)
+    #inhibit_control = nengo.Node([0])
     #scale_control = nengo.Node([0]*pattern_num)
 
-    output = nengo.networks.EnsembleArray(n_neurons=100, n_ensembles=output_dims,
-                                 label="output")
-
+    output = nengo.networks.EnsembleArray(n_neurons=1, n_ensembles=output_dims, radius=np.pi,
+                                          neuron_type=nengo.Direct(), label="output")
     # one ensemble array per output pattern
     # each ensemble array has the output dimensions
     # combine ensemble arrays to combine patterns
@@ -92,27 +90,19 @@ with model:
     for n_i, nm in enumerate(pattern_file_names):
         # make the EnsembleArray with the associated functions
         name = nm[5:]
-        e = nengo.networks.EnsembleArray(n_neurons=100, n_ensembles=output_dims,
-                                label=name)
-        """
-        for dim in range(output_dims):
-            e.add_output("out_"+name+str(dim), function_list[n_i][dim])
-        """
+        e = nengo.networks.EnsembleArray(n_neurons=ea_n_neurons, n_ensembles=output_dims,
+                                         radius=np.pi, label=name)
         e.add_output("out_"+name, function_list[n_i])
         e.add_neuron_input()
 
         # make the connections for inhibition (proxy for output of BG)
-        nengo.Connection(inhibit_control[n_i], e.neuron_input,
-                         transform=np.ones((100*output_dims, 1)) * -3)
+        #nengo.Connection(inhibit_control[n_i], e.neuron_input,
+        #                 transform=np.ones((100*output_dims, 1)) * -3)
 
         # TODO: make the connections for scaling (proxy for output of Thal)
 
         # make input and output connections
         nengo.Connection(readout, e.input, transform=np.ones((output_dims, 1)))
-        """
-        for dim in range(output_dims):
-            nengo.Connection(getattr(e, "out_"+name+str(dim)), output.input[dim])
-        """
         nengo.Connection(getattr(e, "out_"+name), output.input)
 
         ea_list.append(e)
